a25c66c8c19af714790be10750145191506388e0
Emelie Almqvist <emeline.andersson@live.se>
2020-10-07 15:21:44 +0200

    Added view temp for a whole month

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-def User():
-    pass
-
-def Degreed():
-    pass
+import csv
 
 
 def main():
-    pass
+
+    with open("ljusnedal_date_time.csv", "r") as file:
+        text = csv.reader(file)
+
+        next(text)
+
+        for line in text:
+            split_line = line[0].split(";")
+            date = split_line[0]
+            temp = float(split_line[1])
+
+            while "2010-10-01" <= date <= "2010-10-31":
+                print(date, "the temp was: ", temp)
+                break
 
 
+        # temperatures = []
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
