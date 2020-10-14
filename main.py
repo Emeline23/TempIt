c4b85582b4a47b469fdbf09f5618ab0040560bc5
Emelie Almqvist <emeline.andersson@live.se>
2020-10-14 13:38:59 +0200

    Added accuracy loop

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from temperatures import TemperatureStatistics
 
 def main():
 
-    year = input("Please enter a year (yyyy): ")
     month = int(input("Please enter a month (m): "))
 
-    start_date = year
+    accuracy_number = int(input("Please choose accuracy (number of years back - max 10 yrs): "))
+    start_date = accuracy_number
+
+    year = start_date
     end_date = month
 
-    city1 = TemperatureStatistics(year, month, start_date, end_date)
+    city1 = TemperatureStatistics(year, month, start_date, end_date, accuracy_number)
 
     result = city1.avg_temp()
     print('\n'f"You can expect around {result} {'C°'}")
--- temperatures.py
+++ temperatures.py
@@ -1,24 +1,43 @@
 import csv
 
+"""
+def __str__(self):
+    return f"{self.temp}, {self.date} {self.time}"""
+
 
 class TemperatureStatistics:
-    def __init__(self, year, month, start_date, end_date):
+    def __init__(self, year, month, start_date, end_date, accuracy_number):
+        """self.city = city  # så länge som varje stad har en csv-fil
+        self.date = date
+        self.time = time
+        self.temp = temps
 
-        self.year = year  # TODO set year(instead of user input) here and in a loop
+        city, date, time, temp"""
+
+        self.year = year
         self.month = month
 
         self.start_date = start_date
         self.end_date = end_date
+        self.accuracy_number = accuracy_number
 
-        if self.month < 10:
-            self.start_date = (self.year + "-" + "0" + str(self.month) + "-" + "01")
-        else:
-            self.start_date = (self.year + "-" + str(self.month) + "-" + "01")
+        for self.year in range(2020 - self.accuracy_number, 2020):
+            self.year = (2019 - self.accuracy_number)
+            self.year += 1
+            if self.month < 10:
+                self.start_date = (str(self.year) + "-" + "0" + str(self.month) + "-" + "01")
+            else:
+                self.start_date = (str(self.year) + "-" + str(self.month) + "-" + "01")
 
-        if end_date > 30:
-            self.end_date = (self.year + "-" + str(self.month) + "-" + "31")  # TODO modify 31 days, how reach date in csv-file?
+        months_31 = [1, 3, 5, 7, 8, 10, 12]
+        months_30 = [4, 6, 9, 11]
+
+        if self.month in months_31:
+            self.end_date = (str(self.year) + "-" + str(self.month) + "-" + "31")
+        elif self.month in months_30:
+            self.end_date = (str(self.year) + "-" + str(self.month) + "-" + "30")
         else:
-            self.end_date = (self.year + "-" + str(self.month) + "-" + "30")
+            self.end_date = (str(self.year) + "-" + str(self.month) + "-" + "27")
 
     def avg_temp(self):
         with open("ljusnedal.csv", "r") as file:
